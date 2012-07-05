20e243ba9b8e8da9bf5a7ce75885c44735b22717
G. Blake Meike <blake.meike@gmail.com>
2012-07-05 16:16:47 -0700

    Working DB

--- Yamba/src/com/marakana/yamba/svc/UpdaterService.java
+++ Yamba/src/com/marakana/yamba/svc/UpdaterService.java
@@ -1,21 +1,27 @@
 package com.marakana.yamba.svc;
 
-import java.util.Date;
 import java.util.List;
 
 import winterwell.jtwitter.Twitter;
+import winterwell.jtwitter.Twitter.Status;
 import winterwell.jtwitter.TwitterException;
 import android.app.IntentService;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.provider.BaseColumns;
 import android.util.Log;
 
 import com.marakana.yamba.YambaApplication;
+import com.marakana.yamba.data.TimelineContract;
+import com.marakana.yamba.data.TimelineDao;
 
 
 /**
  * UpdaterService
  */
 public class UpdaterService extends IntentService {
+    public static final long POLL_INTERVAL = 30 * 1000;
+
     private static final String TAG = "UpdaterService";
 
     /** Ctor */
@@ -27,20 +33,52 @@ public class UpdaterService extends IntentService {
     @Override
     protected void onHandleIntent(Intent arg0) {
         Log.v(TAG, "onHandleIntent() invoked");
+        fetchStatusUpdates();
+    }
 
-        try {
-            List<Twitter.Status> timeline
-                = ((YambaApplication) getApplication()).getTwitter().getHomeTimeline();
+    private void fetchStatusUpdates() {
+        Log.d(TAG, "Fetching status updates");
 
-            for (Twitter.Status status: timeline) {
-                String name = status.user.name;
-                String msg = status.text;
-                long id = status.id;
-                Date createdAt = status.createdAt;
-                Log.v(TAG, name + "#" + id + " @" + createdAt + ": " + msg);
+        Twitter twit = ((YambaApplication) getApplication()).getTwitter();
+        if (twit == null) {
+            Log.d(TAG, "Twitter connection info not initialized");
+            return;
+        }
+
+        // http://www.wherever.com/foo/bar?this=that&time=now
+        List<Status> statuses = null;
+        Exception fail = null;
+        try { statuses = twit.getFriendsTimeline(); }
+        catch (NullPointerException e) { fail = e; }
+        catch (TwitterException e) { fail = e; }
+
+        if (null != fail) {
+            Log.e(TAG, "Failed to fetch status updates", fail);
+            return;
+        }
+
+        int nUpdates = addAll(statuses);
+        Log.e(TAG, "New records added: " + nUpdates);
+    }
+
+    private int addAll(List<Status> statuses) {
+        TimelineDao dao = ((YambaApplication) getApplication()).getDao();
+        long mostRecentStatus = dao.getLatestStatusCreatedAtTime();
+        int i = 0;
+        ContentValues values = new ContentValues();
+        for (Status status: statuses) {
+            long t = status.getCreatedAt().getTime();
+            if (mostRecentStatus <= t) {
+                values.clear();
+                values.put(BaseColumns._ID, Long.valueOf(status.getId()));
+                values.put(TimelineContract.Columns.CREATED_AT, Long.valueOf(t));
+                values.put(TimelineContract.Columns.TEXT, status.getText());
+                values.put(TimelineContract.Columns.USER, status.getUser().getName());
+                dao.insertOrIgnore(values);
+                i++;
             }
         }
-        catch (NullPointerException e) { /* handle twitter bug */ }
-        catch (TwitterException e) { Log.w(TAG, "Failed to retrieve timeline data"); }
+
+        return i;
     }
 }
